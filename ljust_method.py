78b2220a4d5eb12342121e8ac527ea5a097fb0f1
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 17:13:38 +0800

    Finished the code for ljust.

--- ljust_method.py
+++ ljust_method.py
@@ -1,4 +1,9 @@
 # how ljust() works
 string = "I should be 50 letters long, but I'm not."
-print(string.ljust(100,"-"))
-print(len(string))
+print(string.ljust(50,"-"))
+
+# how ljust() work without ljust() method
+if len(string) != 50:
+    print(string + "-" * (50 - len(string)))
+else:
+    print(string)
