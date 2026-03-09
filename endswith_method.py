d96a79e3fe947d03b51cf26ed0adb5b91c29dfd0
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 16:48:23 +0800

    More examples.

--- endswith_method.py
+++ endswith_method.py
@@ -1,11 +1,12 @@
 email_address = "me.@gmail.com"
 second_email_address = "you.@gmail.com"
+not_a_gmail_address = "me.@yahoo.com"
 
 # how endswith() works
 print(f"Is '{email_address}' a gmail address? {email_address.endswith('@gmail.com')}")
 
 # how endswith() works without endswith() method
-if "@gmail.com" in second_email_address[-10:]:
-    print(f"Is '{second_email_address}' a gmail address? True")
+if "@gmail.com" in not_a_gmail_address[-10:]:
+    print(f"Is '{not_a_gmail_address}' a gmail address? True")
 else:
-    print(f"Is '{second_email_address}' a gmail address? False")
+    print(f"Is '{not_a_gmail_address}' a gmail address? False")
