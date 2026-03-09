2cb0a18b8a069648c75a7814f7740dc2a80c21bf
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 19:00:22 +0800

    Added more examples for lower.py.

--- lower_method.py
+++ lower_method.py
@@ -1,5 +1,5 @@
 string = "ABCDEFG HI! EHHEHE"
-
+second_string = "AbCdEfG Hi! HeHeHe"
 lower_string = ""
 # how lower() works
 print(string.lower())
@@ -7,7 +7,7 @@ print(string.lower())
 # how lower() works without lower() method
 # can't do without lower() method cause dont know how convert that to numbers then to acii
 #  but i discovered i can use swapcase so im gonna use that 
-for char in string:
+for char in second_string:
     if char.isupper():
         lower_string += char.swapcase()
     else:
