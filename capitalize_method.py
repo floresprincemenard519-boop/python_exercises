aee7ef421af22d8f9184b0ddc509ad7ddcad36be
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 18:02:00 +0800

    Added more examples for capitalize().

--- capitalize_method.py
+++ capitalize_method.py
@@ -1,12 +1,13 @@
 string = "i should have a capital letter at the beginning."
+second_string = "Im the second ehhhehehe."
 
 # how capitalize() works
 print(string.capitalize())
 
 # how capitalize() works without capitalize() method
 
-if string[0].islower():
-    char = string[0].upper()
-    print(char + string[1:])
+if second_string[0].islower():
+    char = second_string[0].upper()
+    print(char + second_string[1:])
 else:
-    print(string)
+    print(second_string)
