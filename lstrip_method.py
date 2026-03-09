4fb5fb5b1c0fefb1e833f2c238b62747bbf2fcb4
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 14:22:55 +0800

    Added steps on what to do and pseudo code.

--- lstrip_method.py
+++ lstrip_method.py
@@ -1,2 +1,11 @@
 # lstrip() example function
 string = "   What a beautiful world! Helloooooo!"
+print(string.lstrip())
+
+# remove spaces without using lstrip()
+
+# check if the characters in string is space or not
+
+# if character is space then remove it from the string
+
+# if encountered a non space dont remove spaces further 
