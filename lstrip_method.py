f3bad44fe92480fcdbee8a15bf071ec6a1847a58
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 14:39:49 +0800

    Added a switch so that the spaces after the first letter is printed and not deleted too

--- lstrip_method.py
+++ lstrip_method.py
@@ -2,14 +2,15 @@
 string = "   What a beautiful world! Helloooooo!"
 # print(string.lstrip())
 spaceless_string = ""
+non_space_detected = False
 # remove spaces without using lstrip()
 
 # check if the characters in string is space or not
+# if character is space then remove it from the string
+# if encountered a non space dont remove spaces further 
 for char in string:
     if char != " ":
         spaceless_string += char
-# if character is space then remove it from the string
-
-# if encountered a non space dont remove spaces further 
+        non_space_detected = True
 
 print(spaceless_string)
