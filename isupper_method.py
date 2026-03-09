7fbc7ef5259a351a09ef723d852b70204a2e62f0
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 16:35:53 +0800

    Added a code that checks if a letter is in the uppercase alphabet

--- isupper_method.py
+++ isupper_method.py
@@ -1,4 +1,12 @@
-string = "abcdefg hi! eheheheh"
+string = "Ah! Hi!"
 
 # how isupper() works
+for char in string:
+    print(f"Is '{char}' uppercase? {char.isupper()}")
 
+# how isupper() works without isupper() method
+for char in string:
+    if 'A' <= char <= 'Z':
+        print(f"Is '{char}' uppercase? True")
+    else:
+        print(f"Is '{char}' uppercase? False")
