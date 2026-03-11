b10f21921864deefe84cb3305b8254b17251ce68
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:33:26 +0800

    Finalized and cleaned the code.

--- quotient_of_two_numbres.py
+++ quotient_of_two_numbres.py
@@ -1,6 +1,4 @@
-# get the first number
 dividend = float(input("Give me the dividend: "))
-# get the second number
 divisor = float(input("Give me the divisor: "))
-# print the quotient
+
 print(dividend / divisor)
