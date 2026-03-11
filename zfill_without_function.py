62e961cf884be5811f083ba6c3526dfb5b159ad7
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 04:06:02 +0800

    finished the code

--- zfill_without_function.py
+++ zfill_without_function.py
@@ -1,3 +1,9 @@
 # get number in str
 # if len of str less than parameter add 0 to front
 # print
+
+numbers = '1231321'
+zfill_parameter = 10
+
+if len(numbers) != zfill_parameter:
+    print((zfill_parameter - len(numbers)) * '0' + numbers)
