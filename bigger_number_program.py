36df12f790a9b0fa91ce506aba0d7088940831ae
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 12:58:25 +0800

    Added conditions involving equal numbers.

--- bigger_number_program.py
+++ bigger_number_program.py
@@ -5,4 +5,7 @@ second_number = float(input("Give me another number: "))
 if first_number > second_number:
 # print bigger number 
     print(f"The bigger number is {first_number}.")
-
+elif second_number > first_number:
+    print(f"The bigger number is {second_number}.")
+else:
+    print(f"The numbers are equal.")
