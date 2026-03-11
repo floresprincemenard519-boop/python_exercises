8ac3d04d7f7beae98f2ff339236eebb331254fe4
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 04:25:05 +0800

    tested the for loop if its correct

--- rindex_without_function.py
+++ rindex_without_function.py
@@ -4,3 +4,7 @@
 # find the first index of parameter
 # print it 
 
+string = "The rindex('p') of this string should print 38."
+print(len(string), end=' ')
+for i in range(len(string), -1, -1):
+    print(i)
