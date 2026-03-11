db6cb8325ef0d08d12f7f6a46dc7d1a24f5fa1c3
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 19:10:56 +0800

    Finished and cleaned the code.

--- numbers_from_0to100_not_ending_in_zero.py
+++ numbers_from_0to100_not_ending_in_zero.py
@@ -1,7 +1,4 @@
-# for loop until 100
 for numbers in range(0,100):
-# check if ending in 0
+
     if numbers % 10 != 0:        
-# if ending in 0 dont print
         print(numbers, end=" ")
-# if no print
