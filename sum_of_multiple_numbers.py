a9a77e1ea11c605015e8d49276d1e3a895a6d39a
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:44:41 +0800

    Followed the pseudocode and tested it with examples.

--- sum_of_multiple_numbers.py
+++ sum_of_multiple_numbers.py
@@ -1,4 +1,8 @@
 # get 1st num
+first_number = float(input("Give me a number: "))
 # get another
+second_number = float(input("Give me another: "))
 # and another
+third_number = float(input("Another: "))
 # sum all of them
+print(first_number + second_number + third_number)
