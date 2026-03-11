6365d83c5385b4f533f2b33848eb64bbcc57ac8c
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 20:55:53 +0800

    finished the while loop idea cleaning is next.

--- numbers_between_two_nums.py
+++ numbers_between_two_nums.py
@@ -1,9 +1,15 @@
 # get two nums
 # get the numbers in the middle using range or maybe i can use a while loop
-first_number = float(input("Please input a number: "))
-second_number = float(input("please input another number: "))
+first_number = int(input("Please input a number: "))
+second_number = int(input("please input another number: "))
 
-if first_number > second_number:
+if first_number > second_number + 1:
     while first_number != second_number:
-        print(first_number)
         first_number -= 1
+        print(first_number, end=" ")
+elif second_number > first_number:
+    while second_number!= first_number + 1:
+        second_number -= 1
+        print(second_number, end=" ")
+else:
+    print("The numbers are equal.")
