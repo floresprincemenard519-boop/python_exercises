3211e14c67f089ed793ce0ce3d8cd64ff71370ab
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 03:07:52 +0800

    finished the code used mutiple for loops to get the spaces in the right side and to print the string as a strait string.

--- rstrip_without_function.py
+++ rstrip_without_function.py
@@ -1,11 +1,14 @@
-# get a string
-# remove space in the right side
-
 string = "What a beautiful world! Helloooooo!    "
+string = list(string)
 
 for index in range(len(string)-1, -1, -1):
     if string[index] == ' ':
-        print('', end='')
-    elif string[index] == ' ':
+        string.pop(-1)
+    else:
         break
-    print(string[index], end='')
+
+# to show that the spaces are removed
+print(string)
+
+for char in string:
+    print(char, end="")
