2575b0e246f57dfb7f80e444b9ac6955588e3b38
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 00:53:33 +0800

    Scrapped the idea for its out of range of the problem.

--- input_number_in_6_digit_format.py
+++ input_number_in_6_digit_format.py
@@ -1,5 +1,3 @@
 # get inputed number from 1 to 1000
 # add 0 in front to make it into 6 digit number
 number = float(input("Please give a number ranging from 0 to 1000: "))
-while len(number) != 4 or len(number) > 4:
-    print()
