5702b7d9e1e84fe07920a6721f15b46a177d9b78
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 12:56:56 +0800

    Pseudocode second step done.

--- bigger_number_program.py
+++ bigger_number_program.py
@@ -2,5 +2,7 @@
 first_number = float(input("Give me a number: "))
 second_number = float(input("Give me another number: "))
 # which of the two numbers is bigger 
-
+if first_number > second_number:
 # print bigger number 
+    print(f"The bigger number is {first_number}.")
+
